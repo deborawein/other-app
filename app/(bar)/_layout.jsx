269d7b0a20776376cab4b06198dd00ed4d780c2b
deborawein <debiwein@gmail.com>
2024-10-17 19:42:28 +1100

    App Structure

---
+++ app/(bar)/_layout.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const BarLayout = () => {
+  return (
+    <View>
+      <Text>BarLayout
+    
+      </Text>
+    </View>
+  )
+}
+
+export default BarLayout
